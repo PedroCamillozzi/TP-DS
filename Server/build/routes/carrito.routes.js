5942e896031b904801b9468dd45e11cef2139364
PedroCamillozzi <pedro_camillozzi@hotmail.com>
2023-11-12 13:06:50 -0300

    Entrega back DS

--- Server/build/routes/carrito.routes.js
+++ Server/build/routes/carrito.routes.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const Carrito_controller_1 = require("../controller/Carrito.controller");
-<<<<<<< HEAD
 const validate_token_controller_1 = __importDefault(require("../controller/validate.token.controller"));
 const routerCarrito = (0, express_1.Router)();
 routerCarrito.get('/:idCliente', validate_token_controller_1.default, Carrito_controller_1.getProductosCliente);
@@ -13,11 +12,4 @@ routerCarrito.patch('/addItem', validate_token_controller_1.default, Carrito_con
 routerCarrito.patch('/addCantidad', validate_token_controller_1.default, Carrito_controller_1.patchAgregarCantidadProductosCliente);
 routerCarrito.delete('/:idCliente/:idProducto', validate_token_controller_1.default, Carrito_controller_1.deleteProductoCliente);
 routerCarrito.delete('/:idCliente', validate_token_controller_1.default, Carrito_controller_1.deleteAllProductoCliente);
-=======
-const routerCarrito = express_1.Router();
-routerCarrito.get('/:idCliente', Carrito_controller_1.getProductosCliente);
-routerCarrito.patch('/addItem', Carrito_controller_1.patchProductoCliente);
-routerCarrito.patch('/addCantidad', Carrito_controller_1.patchAgregarCantidadProductosCliente);
-routerCarrito.delete('/:idCliente/:idProducto', Carrito_controller_1.deleteProductoCliente);
->>>>>>> a07ca4f7a07201d29d70c95e7f1ff4df0b2e41fa
 exports.default = routerCarrito;
--- Server/build/routes/detallePedido.routes.js
+++ Server/build/routes/detallePedido.routes.js
@@ -5,13 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const detallePedido_controller_1 = require("../controller/detallePedido.controller");
-<<<<<<< HEAD
 const validate_token_controller_1 = __importDefault(require("../controller/validate.token.controller"));
 const routerDetallePedido = (0, express_1.Router)();
 routerDetallePedido.get('/:idPedido', validate_token_controller_1.default, detallePedido_controller_1.getDetallePedidosCliente);
 routerDetallePedido.post('/', validate_token_controller_1.default, detallePedido_controller_1.postDetallePedido);
-=======
-const routerDetallePedido = express_1.Router();
-routerDetallePedido.get('/:idPedido', detallePedido_controller_1.getDetallePedidosCliente);
->>>>>>> a07ca4f7a07201d29d70c95e7f1ff4df0b2e41fa
 exports.default = routerDetallePedido;
